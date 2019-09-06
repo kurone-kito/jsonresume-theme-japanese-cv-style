2274070fdb8209d952c07106a8b686787d9fe7f1
kurone-kito <krone@kit.black>
2019-09-06 21:45:44 +0900

    Improved a render script

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = { render: () => '<body>Hello</body>' };
+const pug = require('pug');
+const fp = require('lodash/fp');
+
+module.exports = {
+  render: resume =>
+    pug.renderFile(
+      './pug/index.pug',
+      fp.merge(resume)({
+        basics: {
+          date: new Date().toLocaleDateString('ja-JP', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+          })
+        }
+      })
+    )
+};
