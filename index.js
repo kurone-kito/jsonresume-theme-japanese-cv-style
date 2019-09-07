b704220267d9a5df537f1cf9f8bf651898310992
kurone-kito <krone@kit.black>
2019-09-07 23:59:01 +0900

    Improved a theme

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const pug = require('pug');
 const fp = require('lodash/fp');
 
+/** @typedef {import('./schema').ResumeSchema} ResumeSchema */
+
+const dateFormatter = Intl.DateTimeFormat('ja-JP', {
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric'
+});
+
 module.exports = {
-  render: resume =>
-    pug.renderFile(
-      './pug/index.pug',
-      fp.merge(resume)({
-        basics: {
-          date: new Date().toLocaleDateString('ja-JP', {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-          })
-        }
-      })
-    )
+  /** @type {(resume: ResumeSchema) => string} */
+  render: ({ skills, work, ...resume }) => {
+    const sortedSkills = fp
+      .sortBy(({ category }) => category)(skills)
+      .reverse();
+    const categories = fp.groupBy(({ category }) => category)(skills);
+    /** @type {ResumeSchema} */
+    const additional = {
+      basics: { date: dateFormatter.format(Date.now()) },
+      work: [
+        ...(work || []).map(({ endDate, startDate, ...element }) => ({
+          ...element,
+          endDate:
+            (endDate && dateFormatter.format(new Date(endDate))) || '現在',
+          startDate: startDate && dateFormatter.format(new Date(startDate))
+        }))
+      ],
+      skills: [...sortedSkills]
+    };
+    return pug.renderFile('./pug/index.pug', fp.merge(resume)(additional));
+  }
 };
