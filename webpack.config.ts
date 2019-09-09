6c4031fdbc3455c631ea842807f61d9a620aa8c5
kurone-kito <krone@kit.black>
2019-09-10 00:50:32 +0900

    Added setttings for Webpack

---
+++ webpack.config.ts
@@ -0,0 +1,39 @@
+import webpack from 'webpack';
+
+const staticSettings: webpack.Configuration = {
+  cache: true,
+  devtool: false,
+  entry: './src/index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          configFile: '.eslintrc.yml'
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'index.js',
+    path: __dirname,
+    library: 'jsonresume-theme-japanese-cv-style',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.ts', '.tsx']
+  },
+  target: 'node'
+};
+
+export default (source: webpack.Configuration): webpack.Configuration => ({
+  ...source,
+  ...staticSettings
+});
